4e29ea9f34a3483575eaad2d15ab09e9a33e2b15
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-02-25 17:13:53 -0300

    Criando Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles'
+import Layout from './components/Layout'
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <> 
+    <Layout/>
+    <GlobalStyles/>
+    </>
+  )
 }
 
 export default App;
